b5a8065fa2a81c3d033ecf6bd104a86d8698a642
matijagu <matija@amondi-media.com>
2022-01-20 13:28:09 +0000

    msg

---
+++ test-module/models/models.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class Partner(models.Model):
+     _inherit = 'res.partner'
+#     _description = 'test-module.test-module'
+
+     testField = fields.Char('Test Field')
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
