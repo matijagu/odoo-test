c5162aeac2e6b01242b96d753717e3b22b9a04f8
matijagu <matija@amondi-media.com>
2022-01-21 12:24:57 +0000

    OpenAcademy module - not working yet [menu item not showing up]

---
+++ openacademy/models/.ipynb_checkpoints/models-checkpoint.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+#we are creating model COURSE under our openacademy module
+#
+
+class Course(models.Model):
+    _name = 'openacademy.course'
+    _description = 'OpenAcademy Courses'
+    
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
+
+#class openacademy(models.Model):
+#    _name = 'openacademy.openacademy'
+#    _description = 'openacademy.openacademy'
+#    name = fields.Char()
+#    value = fields.Integer()
+#    value2 = fields.Float(compute="_value_pc", store=True)
+#    description = fields.Text()
+#
+#    @api.depends('value')
+#    def _value_pc(self):
+#        for record in self:
+#            record.value2 = float(record.value) / 100
